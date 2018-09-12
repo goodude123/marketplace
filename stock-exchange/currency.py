789fd68abd2075fcfb6c3cd7c8976996bfa41a52
goodude123 <assasinmcpl@gmail.com>
2018-09-13 00:36:20 +0200

    Currencies can save in folders (every currency got own folder in which are: json file with constants, csv file with rates and dates)

--- stock-exchange/currency.py
+++ stock-exchange/currency.py
@@ -1,6 +1,9 @@
 from request_handle import simple_get
 from bs4 import BeautifulSoup
 from datetime import datetime
+import os
+import json
+import pandas as pd
 
 class Currency:
     """ Simple currency class """
@@ -54,12 +57,41 @@ class Data:
                     args.append(str(date)) 
 
                     self.currencies.append(Currency(*args))
+
+    def save_currencies(self):
+        path = 'currencies/'
+        for currency in self.currencies:
+            directory = path + currency.code
+            # if directory for each currency doesnt exists make it
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            
+            info_path = directory + '/information.json'
+            # if info file doesnt exists make it (save in json currency obj)
+            if not os.path.exists(info_path):
+                with open(info_path, 'w') as file:
+                    info = [currency.name, currency.value, currency.code]
+                    json.dump(info, file)
+                file.close()
+
+            csv_path = directory + '/' + currency.code + '.csv'
+            data = [[currency.course, currency.date]]
+            print(data)
+            if not os.path.exists(csv_path): 
+                with open(csv_path, 'w') as file:
+                    df = pd.DataFrame(data, columns=['Rate', 'Date'])
+                    df.to_csv(file, index=False)
+                file.close()
+            else:
+                df = pd.DataFrame(data, columns=['Rate', 'Date'])
+                with open(csv_path, 'a') as file: 
+                    df.to_csv(file, header=False, index=False)
+                file.close()
                     
                     
 
 if __name__ == '__main__':
     obj = Data()
     obj.get_currencies()
-    for obj in obj.currencies:
-        print(obj.name, obj.value, obj.code, obj.course, obj.date)
+    obj.save_currencies()
         
