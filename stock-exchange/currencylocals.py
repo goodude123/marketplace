73482b25c66cdaed6f3b13b23b763c2694aac573
goodude123 <assasinmcpl@gmail.com>
2018-09-17 22:24:47 +0200

    New class for Currencies read from local data in files

---
+++ stock-exchange/currencylocals.py
@@ -0,0 +1,39 @@
+import os
+import json
+import pandas as pd
+
+
+class LocalCurrency:
+    def __init__(self, abbr):
+        self.path = 'currencies/'
+        self.abbr = abbr
+        self.currency_directory_path = self.path + self.abbr + '/'
+
+    def get_properties_from_json(self):
+        """ Gets values from json file """
+        properties_json_file = self.currency_directory_path + 'properties.json'
+        if os.path.isfile(properties_json_file):
+            with open(properties_json_file) as json_file:
+                currency_info = json.load(json_file)
+            json_file.close()
+            return currency_info
+
+    def get_rates_from_csv_as_dataframe(self):
+        """ Gets courses from csv file and return it as DataFram """
+        rates_csv_file = self.currency_directory_path + self.abbr + '.csv'
+        if os.path.isfile(rates_csv_file):
+            with open(rates_csv_file) as csv_file:
+                currency_rates_as_dataframe = pd.read_csv(csv_file)
+            csv_file.close()
+            return currency_rates_as_dataframe
+
+    def is_main_directory(self):
+        """ Check is main directory exists """
+        main_directory = self.path + self.abbr
+        return os.path.isdir(main_directory)
+
+    def if_main_dir_assign_(self):
+        """ Assign currency data (rates, information) to object """
+        if self.is_main_directory():  # checks is currency folder exists
+            self.currency_info = self.get_properties_from_json()
+            self.currency_rates = self.get_rates_from_csv_as_dataframe()
