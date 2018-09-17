7d97b29c7dc29adeb6b12ba6ed0085566d168c28
goodude123 <assasinmcpl@gmail.com>
2018-09-17 21:54:35 +0200

    get_rates function separated into many functions

--- stock-exchange/plot.py
+++ stock-exchange/plot.py
@@ -6,45 +6,45 @@ import matplotlib.pyplot as plt
 
 class Plot:
     """ Plot object, allow to create and show diagrams, graphs. """
-
-    def create_path(self, directory, file_name, file_extension):
-        path = directory + '/' + file_name + file_extension
-        return path
-
-    def save_properties_in_json_file(self):
-        with open(properties_path, mode) as property_file:
-            properties = property_list
-            json.dump(properties, property_file)
-        property_file.close()
-
     def __init__(self, abbr):
         self.currency_rates = None
         self.currency_info = None
         self.path = 'currencies/'
         self.abbr = abbr
+        self.currency_directory_path = self.path + self.abbr + '/'
 
-    def read_data(self):
-        """ Reads choosen currency data (rates, information) """
+    def get_properties_from_json(self):
+        """ Gets values from json file """
+        properties_json_file = self.currency_directory_path + 'properties.json'
+        if os.path.isfile(properties_json_file):
+            with open(properties_json_file) as json_file:
+                currency_info = json.load(json_file)
+            json_file.close()
+            return currency_info
+
+    def get_rates_from_csv_as_dataframe(self):
+        """ Gets courses from csv file and return it as DataFram """
+        rates_csv_file = self.currency_directory_path + self.abbr + '.csv'
+        if os.path.isfile(rates_csv_file):
+            with open(rates_csv_file) as csv_file:
+                currency_rates_as_dataframe = pd.read_csv(csv_file)
+            csv_file.close()
+            return currency_rates_as_dataframe
+
+    def is_main_directory(self):
+        """ Check is main directory exists """
         main_directory = self.path + self.abbr
-        if os.path.isdir(main_directory):  # checks is currency folder exists
-            json_path = main_directory + '/properties.json'
-            courses_file_currency = main_directory + '/' + self.abbr + '.csv'
-            if os.path.isfile(json_path):  # checks is json folder exists
-                with open(json_path) as json_file:
-                    self.currency_info = json.load(json_file)
-                json_file.close()
+        return os.path.isdir(main_directory)
 
-            # checks is csv folder exists
-            if os.path.isfile(courses_file_currency):
-                with open(courses_file_currency) as csv_file:
-                    data_read_from_file = pd.read_csv(csv_file)
-                    self.currency_rates = data_read_from_file
-                csv_file.close()
+    def if_main_dir_assign_(self):
+        """ Assign currency data (rates, information) to object """
+        if self.is_main_directory():  # checks is currency folder exists
+            self.currency_info = self.get_properties_from_json()
+            self.currency_rates = self.get_rates_from_csv_as_dataframe()
 
     def create_graph(self):
         """ Creates and show graph from data """
         self.read_data()
-        # self.currency_rates.plot(x='Date', y='Rate')
         date_list = self.currency_rates['Date'].tolist()
         rates_list = self.currency_rates['Rate'].tolist()
 
