32c5f8d77db913d74e355754d7f9786835fcc0cf
goodude123 <assasinmcpl@gmail.com>
2018-09-27 22:58:10 +0200

    Refactored file structure, added adsheduler, modified views (some converted to generic), added template

---
+++ marketplace/stock_exchange/managment/commands/_currency.py
@@ -0,0 +1,11 @@
+class Currency:
+    """ Simple currency class """
+
+    def __init__(self, name, unit, code, course, date):
+        self.name = name
+        self.unit = unit
+        self.code = code
+        self.course = course
+        self.date = date
+
+
---
+++ marketplace/stock_exchange/managment/commands/scrap.py
@@ -0,0 +1,48 @@
+from django.core.managment.base import BaseCommand
+
+from .request_handle import simple_get
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+class Command(BaseCommand):
+    def __init__(self):
+        """ Create empty data object """
+
+        self.url = 'https://www.nbp.pl/home.aspx?f=/kursy/kursya.html'
+        self.currencies = []
+
+    def create_html_code(self):
+        """ Creates bs4 html code model """
+
+        raw_html = simple_get(self.url)
+        bs4_html = BeautifulSoup(raw_html, 'html.parser')
+
+        return bs4_html
+
+    def get_currency_properties(self, table_cells):
+        """ Gets values from table_cells """
+        name = table_cells[0].contents[0]
+        unit, abbreviation = table_cells[1].contents[0].split()
+        rate = float(table_cells[2].contents[0].replace(',', '.'))
+        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        return [name, unit, abbreviation, rate, date]
+
+    def get_currencies(self):
+        """ Finds data in bs4 html, get contents and creates Currency models"""
+
+        html = self.create_html_code()
+        for table in html.findAll('table'):
+
+            # find correct table
+            if table.parent.get('id') == 'article':
+
+                # iterate through table rows
+                for table_row in table.findAll('tr')[3:38]:
+                    table_data_cells = table_row.findAll('td')
+                    currency_properties = Currency(
+                        *self.get_currency_properties(table_data_cells)
+                    )
+
+                    self.currencies.append(currency_properties)
