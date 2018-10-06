d01ae5b3c1adbed5922dd414ff743255e5a097a4
goodude123 <assasinmcpl@gmail.com>
2018-10-06 13:09:21 +0200

    Added url for currency table view.

--- stock_exchange/urls.py
+++ stock_exchange/urls.py
@@ -5,6 +5,7 @@ app_name = 'exchange'
 
 urlpatterns = [
     path('', views.MainPage.as_view(), name='main'),
+    path('table/', views.TableCurrenciesPage.as_view(), name='table'),
     path('currency/<str:abbr>/', views.single_currency_page, name='currency'),
     path('converter/', views.currency_converter, name='converter'),
 ]
