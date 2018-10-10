cb57dd89ecebff0ec70c33f7748ab72d1b903ffd
goodude123 <assasinmcpl@gmail.com>
2018-10-10 19:38:25 +0200

    Added two views: api for data charts, view for charts/

--- stock_exchange/urls.py
+++ stock_exchange/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('table/', views.TableCurrenciesPage.as_view(), name='table'),
     path('currency/<str:abbr>/', views.single_currency_page, name='currency'),
     path('converter/', views.currency_converter, name='converter'),
+    path('api/chart/<str:abbr>/', views.api_chart_data, name='api-data'),
+    path('chart/<str:abbr>', views.chart, name='chart')
 ]
