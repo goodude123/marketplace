79d7fc69e5cb13cc6f79197b0f037cae64e42205
goodude123 <assasinmcpl@gmail.com>
2018-10-13 13:38:06 +0200

    Added traiding app, template main dir for all apps, login and logout redirect URL

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -34,7 +34,8 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'stock_exchange.apps.StockExchangeConfig',
+    'stock_exchange.apps.StockExchangeConfig',  # custom app
+    'trading_app.apps.TradingAppConfig',        # custom app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'trading_app:home'
+LOGOUT_REDIRECT_URL = 'trading_app:home'
