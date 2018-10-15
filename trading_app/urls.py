202252211a3cf91c0b7131549e723a7d40ca4042
goodude123 <assasinmcpl@gmail.com>
2018-10-15 23:56:15 +0200

    Changed to generic views

--- trading_app/urls.py
+++ trading_app/urls.py
@@ -5,6 +5,6 @@ from trading_app import views
 app_name = 'trading_app'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('user/', views.user, name='user')
+    path('', views.Home.as_view(), name='home'),
+    path('user/', views.User.as_view(), name='user')
 ]
--- trading_app/views.py
+++ trading_app/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
-def home(request):
-    return render(request, 'home.html')
+class Home(TemplateView):
+    template_name = 'home.html'
 
 
-@login_required
-def user(request):
-    return render(request, 'user.html')
+@method_decorator(login_required, name='dispatch')
+class User(TemplateView):
+    template_name = 'user.html'
