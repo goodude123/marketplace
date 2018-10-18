72ac71ad468558f8a896676f1d01f14aaa69408d
goodude123 <assasinmcpl@gmail.com>
2018-10-18 19:15:02 +0200

    Added signup view with added custom decorator (prevent logged users)

--- trading_app/views.py
+++ trading_app/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.contrib.auth import login, authenticate
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from trading_app.forms import SignUpForm
+from trading_app.decorators import prevent_logged_users
 
 
 class Home(TemplateView):
@@ -11,3 +15,20 @@ class Home(TemplateView):
 @method_decorator(login_required, name='dispatch')
 class User(TemplateView):
     template_name = 'user.html'
+
+
+@prevent_logged_users
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()  # user is already saved
+            print(form.cleaned_data.get('username'))
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect(reverse('trading_app:home'))
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
