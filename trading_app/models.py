3aa2d318543b8c8d96f9d9988c32730f295f9c64
goodude123 <assasinmcpl@gmail.com>
2018-10-25 20:06:34 +0200

    Added methods to operate on currencies to Profile model and methods comments.

--- trading_app/models.py
+++ trading_app/models.py
@@ -8,20 +8,26 @@ from trading_app import errors
 
 
 class Profile(models.Model):
+    "Extended user profile."
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    money = models.IntegerField(default=100)
+    money = models.FloatField(default=100)
 
     def __str__(self):
         return self.user.username
 
-    def buy(self, to_bought, quantity):
-        rate = self.get_last_rate(to_bought)
+    def buy(self, to_bought_index, quantity):
+        "Substracs price from money and adds currencies to owned."
+
+        rate = self.get_last_rate(to_bought_index)
         price = rate * quantity
+
+        code = self.get_currency_code(to_bought_index)
 
         if price <= self.money:
             self.money -= price
-            try: 
-                already_bought = self.boughtcurrency_set.filter(currency_abbreviation=to_bought.upper())[0]
+            self.save()
+            try:
+                already_bought = self.boughtcurrency_set.filter(currency_abbreviation=code)[0]
             except IndexError:
                 already_bought = False
 
@@ -30,35 +36,47 @@ class Profile(models.Model):
                 already_bought.save()
             else:
                 buys = self.boughtcurrency_set.create(
-                                        currency_abbreviation=to_bought.upper(),
-                                        amount=quantity
-                                        )
+                                        currency_abbreviation=code.upper(),
+                                        amount=quantity)
                 buys.save()
         else:
             raise errors.BuyingError('Too high price!')
 
-    def sell(self, to_sell, quantity):
-        rate = self.get_last_rate(to_sell) - 0.004
+    def sell(self, index_to_database, quantity):
+        "Deletes currencies from profile owned, adding money."
+
+        rate = self.get_last_rate(index_to_database) - 0.004
+        code = self.get_currency_code(index_to_database)
 
         try:
-            owned_currency = self.boughtcurrency_set.get(currency_abbreviation=to_sell.upper())
-            if owned_currency.amount >= quantity:
-                owned_currency.amount -= quantity
-                owned_currency.save()
+            owned_selling_currency = self.boughtcurrency_set.get(currency_abbreviation=code)
+            if owned_selling_currency.amount >= quantity:
+                owned_selling_currency.amount -= quantity
+                owned_selling_currency.save()
                 self.money += rate * quantity
             else:
                 raise errors.SellingError('You dont have enough currencies')
         except ObjectDoesNotExist:
             print('You never had that currency.')
 
-    def get_last_rate(self, currency_code):
-        currency_bought = Currency.objects.get(code=currency_code.upper())
-        rate = currency_bought.rate_and_date_set.all().order_by('-date')[0].rate
+    def get_currency_from_database(self, currency_index):
+        currency = Currency.objects.get(pk=currency_index)
+        return currency
+
+    def get_currency_code(self, currency_index):
+        currency = self.get_currency_from_database(currency_index)
+        return currency.code
+
+    def get_last_rate(self, currency_index):
+        bought_currency = self.get_currency_from_database(currency_index)
+        rate = bought_currency.rate_and_date_set.all().order_by('-date')[0].rate
 
         return rate
 
 
 class BoughtCurrency(models.Model):
+    "Currency already bought by user."
+
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     currency_abbreviation = models.CharField(max_length=8)
     amount = models.IntegerField()
