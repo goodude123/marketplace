2ddce1b0ed380cd2926dd437060924bf6abf5d4c
goodude123 <assasinmcpl@gmail.com>
2018-10-25 20:08:20 +0200

    Added paths to owned currency view and to buying view.

--- trading_app/urls.py
+++ trading_app/urls.py
@@ -7,5 +7,7 @@ app_name = 'trading_app'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('user/', views.User.as_view(), name='user'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('owned/', views.owned_currencies, name='owned_currencies'),
+    path('buy/', views.buy_currencies, name='buy')
 ]
