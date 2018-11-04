0a1f3ec03d25452843b3c3d5ac5421a53240c9b1
goodude123 <assasinmcpl@gmail.com>
2018-11-04 17:06:18 +0100

    Added form for selling currencies:   - as choices takes owned currencies   - initial currency is send in url argument
    Refactored BuyCurrencyForm:
      - added two methods:
        - find_initial_index
        - get_all_currencies

--- trading_app/forms.py
+++ trading_app/forms.py
@@ -18,13 +18,48 @@ class BuyCurrencyForm(forms.Form):
     def __init__(self, initial_code='USD', *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        all_currencies = [(currency.pk, currency.code) for currency in Currency.objects.all()]
-
-        # Find index by currency code from url
-        initial_index = 1
-        for index, code in dict(all_currencies).items():
-            if code == initial_code:
-                initial_index = index
+        self.initial_code = initial_code
 
         self.fields['quantity'] = forms.IntegerField(min_value=1)
-        self.fields['currency_code'] = forms.ChoiceField(choices=all_currencies, initial=initial_index)
+        self.fields['currency_code'] = forms.ChoiceField(choices=self.get_all_currencies(), initial=self.find_initial_index())
+
+    def find_initial_index(self):
+        '''Find index by currency code from url'''
+
+        initial_index = 1
+        for index, code in dict(self.get_all_currencies()).items():
+            if code == self.initial_code:
+                initial_index = index
+
+        return initial_index
+
+    def get_all_currencies(self):
+        all_currencies = [(currency.pk, currency.code) for currency in Currency.objects.all()]
+
+        return all_currencies
+
+
+class SellCurrencyForm(forms.Form):
+    def __init__(self, initial_code=None, user=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.user = user
+        self.initial_code = initial_code
+
+        choices = self.create_index_code_pairs_in_list
+
+        self.fields['quantity'] = forms.IntegerField(min_value=1)
+        self.fields['currency_code'] = forms.ChoiceField(choices=choices)
+
+    def create_index_code_pairs_in_list(self):
+        '''Creates pairs index currency code in list'''
+
+        owned_currencies = self.user.profile.boughtcurrency_set.all()
+        choices = []
+        i = 0
+        for currency in owned_currencies:
+            i += 1
+            code = currency.currency_abbreviation
+            choices.append((i, code))
+
+        return choices
