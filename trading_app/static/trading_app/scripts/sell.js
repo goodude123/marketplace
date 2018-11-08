f311d6aae8eec8b66547f2f4b582295fc60aa45d
goodude123 <assasinmcpl@gmail.com>
2018-11-08 22:55:48 +0100

    Refactored, total price script with module pattern.

--- trading_app/static/trading_app/scripts/sell.js
+++ trading_app/static/trading_app/scripts/sell.js
@@ -1,22 +1,21 @@
 function calculateAndPrintTotalPrice(rates, units){
-    quantity = getQuantityFromElement('id_quantity');
+    quantity = TotalPrice.getQuantityFromElement('id_quantity');
     index = document.getElementById('id_currencies').value;
-    console.log(rates);
     rate = rates[index] - 0.004;
     unit = units[index];
     if (quantity > 0) {
         price = quantity * rate * unit;
         price = price.toFixed(5);
-        price = removeTrailingZeros(price);
+        price = TotalPrice.removeTrailingZeros(price);
         response = 'Total Price: ' + price;
     } else {
         response = 'Invalid Value.';
     }
-    insertTotalPriceIntoId('totalPrice', response);
+    TotalPrice.insertTotalPriceIntoId('totalPrice', response);
 }
 
 
 window.onload = function(){
-    addChangeEventToElementId('id_quantity');
-    addChangeEventToElementId('id_currencies');
+    TotalPrice.addChangeEventToElementId('id_quantity');
+    TotalPrice.addChangeEventToElementId('id_currencies');
 };
