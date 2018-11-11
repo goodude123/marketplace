6f16ed8f572b1e8ebbb818e158a8b6e67f96990e
goodude123 <assasinmcpl@gmail.com>
2018-11-11 14:48:15 +0100

    Separated into smaller functions.

--- stock_exchange/system/pack.py
+++ stock_exchange/system/pack.py
@@ -5,16 +5,30 @@ def pack_currencies(currencies):
     currencies_to_template = []
 
     for currency in currencies:
-        name = currency.name
-        code = currency.code
-        unit = currency.unit
-        last_rate = currency.rate_and_date_set.all().latest('date').rate
-        last_date = currency.rate_and_date_set.all().latest('date').date
-
-        currenct_currency = SingleCurrency(
-            name, unit, code, last_rate, last_date)
-
-        # currencies in list ()
-        currencies_to_template.append(currenct_currency)
+        current_currency = create_single_currency(currency)
+        currencies_to_template.append(current_currency)
 
     return currencies_to_template
+
+
+def create_single_currency(currency):
+    name = currency.name
+    code = currency.code
+    unit = currency.unit
+    last_rate = get_last_rate(currency)
+    last_date = get_last_date(currency)
+
+    currency = SingleCurrency(
+        name, unit, code, last_rate, last_date)
+
+    return currency
+
+
+def get_last_rate(currency):
+    last_rate = currency.rate_and_date_set.all().latest('date').rate
+    return last_rate
+
+
+def get_last_date(currency):
+    last_date = currency.rate_and_date_set.all().latest('date').date
+    return last_date
