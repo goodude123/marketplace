b3717a187a0b1fc6902ed2734d5d641ee65e0f84
goodude123 <assasinmcpl@gmail.com>
2019-04-15 17:58:48 +0200

    Had to add try clausule to make initial migration possibly.

--- stock_exchange/forms.py
+++ stock_exchange/forms.py
@@ -3,7 +3,10 @@ from .models import Currency
 
 
 class CurrencyConverterForm(forms.Form):
-    all_currencies = [(currency.pk, currency.code) for currency in Currency.objects.all()]
-    amount = forms.IntegerField(min_value=1)
-    from_currency = forms.ChoiceField(label='From', choices=all_currencies)
-    to_currency = forms.ChoiceField(label='To', choices=all_currencies)
+    try:
+        all_currencies = [(currency.pk, currency.code) for currency in Currency.objects.all()]
+        amount = forms.IntegerField(min_value=1)
+        from_currency = forms.ChoiceField(label='From', choices=all_currencies)
+        to_currency = forms.ChoiceField(label='To', choices=all_currencies)
+    except Exception as e:
+        print(e)
