99586c33d0550dc32acd16704677a8e480df06b1
goodude123 <assasinmcpl@gmail.com>
2019-04-15 16:58:41 +0200

    Changed function views to CBV.

--- trading_app/urls.py
+++ trading_app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('user/', views.User.as_view(), name='user'),
     path('sign_up/', views.SignUpView.as_view(), name='signup'),
-    path('owned/', views.owned_currencies, name='owned_currencies'),
-    path('buy/<str:initial>/', views.buy_currencies, name='buy'),
-    path('sell/<str:initial>/', views.sell_currencies, name='sell'),
+    path('owned/', views.OwnedCurrenciesView.as_view(), name='owned_currencies'),
+    path('buy/<str:initial>/', views.BuyCurrencyView.as_view(), name='buy'),
+    path('sell/<str:initial>/', views.SellCurrencyView.as_view(), name='sell'),
     path('api/currencies/', api_views.api_currencies, name='api-currencies'),
 ]
