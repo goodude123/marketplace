57dc9beb361fda19efe57506a49190d6107112e3
goodude123 <assasinmcpl@gmail.com>
2019-04-15 15:57:00 +0200

    Decorator for CBV.

--- trading_app/decorators.py
+++ trading_app/decorators.py
@@ -12,3 +12,12 @@ def prevent_logged_users(function):
     wrapper.__name__ = function.__name__
 
     return wrapper
+
+
+def prevent_logged_users_class_decorator(function):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('trading_app:home'))
+        else:
+            return function(request, *args, **kwargs)
+    return wrapper
